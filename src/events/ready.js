3227172aaeb8494a10ae767c5638e7af3f86ac1e
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2020-07-18 13:05:44 +0800

    memory sweeping

--- src/events/ready.js
+++ src/events/ready.js
@@ -12,11 +12,6 @@ module.exports = class extends BaseEvent {
      */
     async run(client) {
         await client.wait(5000);
-        client.setInterval(() => {
-            const c1 = client.users.cache.sweep(() => Math.random() < .90);
-            client.logger.info(`${c1} users sweeped!`);
-            const c2 = client.emojis.cache.sweep((e) => e.guild.id !== process.env.BOT_SERVER_ID && e.guild.id !== '603558917087428618');
-            client.logger.info(`${c2} emojis sweeped!`);
-        }, 60000 * 10);
+        client.startSweepInterval();
     }
 }
