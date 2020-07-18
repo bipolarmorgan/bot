5fac9846d3c55320516394f224329e8c528c0533
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2020-07-18 11:06:00 +0800

    users cache sweeping for memory improvements

--- src/managers/POSTManager.js
+++ src/managers/POSTManager.js
@@ -12,7 +12,7 @@ module.exports = class POSTManager {
         this.server = server;
     }
     /**
-     * @returns {Object<string, number>}
+     * @returns {Promise<Object<string, number>>}
      */
     async getStats() {
         return {
@@ -35,6 +35,8 @@ module.exports = class POSTManager {
                 const { server_count, member_count, shard_count } = await this.getStats();
                 await this.post({ service, server_count, shard_count, member_count });
             }
+            const count = await this.server.manager.broadcastEval(`this.users.cache.sweep(() => Math.random() < .65)`).catch(() => { });
+            this.server.logger.info(`${count.reduce((acc, cur) => acc + cur, 0)} users sweeped!`);
         }, 60000 * 30);
     }
 
@@ -53,20 +55,15 @@ module.exports = class POSTManager {
         if (!options.service || !options.service.token) return;
         try {
             const url = options.service.endpoint.replace(/:id/g, this.server.id);
-            const response = await fetch.default(url,
-                {
-                    method: 'POST',
-                    headers: {
-                        Authorization: options.service.token,
-                        'Content-type': 'application/json',
-                        'User-Agent': 'Unicron LLC'
-                    },
-                    body: JSON.stringify(options.service.parse(options.server_count, options.shard_count, options.member_count)),
-                }
-            );
-            this.server.logger.info(`${url}: Status (${response.status})`);
-        } catch (e) {
-            this.server.logger.error(`${options.service.endpoint}: ${e.message}`);
-        }
+            await fetch.default(url, {
+                method: 'POST',
+                headers: {
+                    Authorization: options.service.token,
+                    'Content-type': 'application/json',
+                    'User-Agent': 'Unicron LLC'
+                },
+                body: JSON.stringify(options.service.parse(options.server_count, options.shard_count, options.member_count)),
+            }).catch(() => { });
+        } catch (e) { }
     }
 }
