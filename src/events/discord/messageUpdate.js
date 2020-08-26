dea44ff56a3972ea660f9f62f0e22059a73363de
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2020-08-26 12:04:33 +0800

    this thing

--- src/events/discord/messageUpdate.js
+++ src/events/discord/messageUpdate.js
@@ -3,7 +3,6 @@ const Event = require('../../classes/DiscordEvent');
 const inviteFilter = require('../../filters/inviteFilter');
 const mentionSpamFilter = require('../../filters/mentionSpamFilter');
 const swearFilter = require('../../filters/swearFilter');
-const memberVerification = require('../../modules/Verification');
 
 module.exports = class extends Event {
     constructor() {
@@ -19,7 +18,6 @@ module.exports = class extends Event {
         if (!newMessage) return;
         const guildSettings = await client.db.guilds.fetch(message.guild.id).catch(console.log);
         message.author.permLevel = client.permission.level(message);
-        if (await memberVerification(client, message, guildSettings)) return;
         if (await swearFilter(client, message, guildSettings)) return;
         if (await inviteFilter(client, message, guildSettings)) return;
         if (await mentionSpamFilter(client, message, guildSettings)) return;
