a0008853fe961d91ab905727fb35d5e0320014d7
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2020-08-28 22:30:17 +0800

    added support for attachments on DMs(modmail)

--- src/events/discord/directMessage.js
+++ src/events/discord/directMessage.js
@@ -3,7 +3,7 @@ const BaseEvent = require('../../classes/DiscordEvent');
 module.exports = class extends BaseEvent {
     constructor() {
         super('directMessage');
-        this.prefix = '**[Moderator]** : ';
+        this.prefix = (tag) => `**Moderator** | ${tag} : `;
     }
     /**
      * 
@@ -17,31 +17,34 @@ module.exports = class extends BaseEvent {
             try {
                 switch (message.content) {
                     case '!!welcome': {
-                        await user.send(this.prefix + 'Hey! :wave: - Give me a minute while I review your request.');
+                        await user.send(this.prefix(message.author.tag) + 'Hey! :wave: - Give me a minute while I review your request.');
                         break;
                     }
                     case '!!all': {
-                        await user.send(this.prefix + 'Is that all you need today?');
+                        await user.send(this.prefix(message.author.tag) + 'Is that all you need today?');
                         break;
                     }
                     case '!!support': {
-                        await user.send(this.prefix + 'Hey! This service is only intended for support! Please do not keep messaging this service.');
+                        await user.send(this.prefix(message.author.tag) + 'Hey! This service is only intended for support! Please do not keep messaging this service.');
                         break;
                     }
                     case '!!require': {
-                        await user.send(this.prefix + 'Do you require any further assistance?');
+                        await user.send(this.prefix(message.author.tag) + 'Do you require any further assistance?');
                         break;
                     }
                     case '!!close': {
-                        await user.send(this.prefix + `I'm going to close this thread now. If you have any further questions, feel free to reach out`);
+                        await user.send(this.prefix(message.author.tag) + `I'm going to close this thread now. If you have any further questions, feel free to reach out`);
                         break;
                     }
                     case '!!unicron': {
-                        await user.send(this.prefix + `we are not Unicron, we are a help system to help you understand something you are confused about with the Bot`);
+                        await user.send(this.prefix(message.author.tag) + `we are not Unicron, we are a help system to help you understand something you are confused about with the Bot`);
                         break;
                     }
                     default: {
-                        await user.send(this.prefix + message.content);
+                        const hasAttachments = message.attachments.size ? true : false;
+                        await user.send(this.prefix(message.author.tag) + message.content, {
+                            attachFiles: hasAttachments && message.attachments.array(),
+                        });
                         break;
                     }
                 }
@@ -50,6 +53,9 @@ module.exports = class extends BaseEvent {
             }
         } else {
             const guild = client.guilds.cache.get('603558917087428618');
+            /**
+             * @type {import('discord.js').TextChannel}
+             */
             let channel = guild.channels.cache.find((c) => c.topic && c.topic === message.author.id && c.parentID === '748855168308609024');
             if (!channel) {
                 channel = await guild.channels.create(message.author.tag.replace(/ +|#/g, '-'), {
@@ -61,7 +67,10 @@ module.exports = class extends BaseEvent {
                 });
             }
             if (!channel) return;
-            channel.send(`**${message.author.tag}** : ${message.content.replace(/@/g, '@' + String.fromCharCode(8203))}`);
+            const hasAttachments = message.attachments.size ? true : false;
+            channel.send(`**User** | ${message.author.tag} : ${message.content.replace(/@/g, '@' + String.fromCharCode(8203))}`, {
+                attachFiles: hasAttachments && message.attachments.array(),
+            });
         }
     }
 }
